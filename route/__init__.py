ea3e7df3366d1925f1b755ce9ec7ab4e7059de53
song <2986715422@qq.com>
2019-01-24 23:00:55 +0800

    Update __init__.py

--- route/__init__.py
+++ route/__init__.py
@@ -7,7 +7,8 @@ from .setTask import *
 from .webssh import *
 from .login import *
 from .linkButton import *
-#from .controlWin import * #windows远程功能并不默认开启,若有需要,可在这里将其导入
+from .plugins import *
+from .controlWin import *
 
 from config.config import NATPenetration
 if NATPenetration:
